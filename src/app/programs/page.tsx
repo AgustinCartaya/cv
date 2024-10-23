9847550bce6b6ccd5afb1ed6641a39f9bf03cc06
danieljml <danielgarrido425@gmail.com>
2024-10-23 17:33:06 -0400

    Refactor some styles in programs

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -7,15 +7,13 @@ import Image from 'next/image'
 const Page = () => {
   const programs = readMeta('/public/programs')
   return (
-    <div className="p-6">
+    <div className="grid gap-4 p-6">
       {programs.map(({ title, description, images, url }) => {
         return (
           <div key={uuidv4()} className="flex flex-col p-6 lg:flex-row gap-4 bg-white rounded-md">
             <div className="flex justify-center">
-              <Image
-                width={300}
-                height={60}
-                className="object-cover rounded-md lg:rounded-l-lg"
+              <img
+                className="w-60 h-55 object-cover rounded-md lg:rounded-l-lg"
                 src={images[0]}
                 alt={images[0]}
               />
@@ -23,7 +21,7 @@ const Page = () => {
             <div className="flex-1 flex flex-col gap-4 justify-between p-4">
               <p className="text-2xl font-bold">{title}</p>
               <p className="text-gray-600" dangerouslySetInnerHTML={{ __html: description }} />
-              <div className="flex justify-center h-full">
+              <div className="flex justify-center">
                 <Link
                   href={`/programs/${url}`}
                   className="text-sm bg-teal hover:bg-dark_teal text-white uppercase px-4 py-2 rounded shadow-md hover:bg-darkBlue transition duration-300"
