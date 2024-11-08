31cc614af591024b9a2ad9bd377c98fa90a1fb20
danieljml <danielgarrido425@gmail.com>
2024-11-08 15:19:47 -0400

    fix: handle relative URLs by appending base URL in verifyURL function

--- src/app/utils/url-validation.ts
+++ src/app/utils/url-validation.ts
@@ -1,4 +1,23 @@
+const getBaseUrl = () => {
+  if (typeof window !== 'undefined') {
+    const { protocol, host } = window.location
+    return `${protocol}//${host}`
+  }
+  return '' // Return an empty string if running on the server
+}
+
 export function verifyURL(text: string): string {
-  const urlRegex = /^(https?:\/\/[^\s/$.?#].[^\s]*)$/i
-  return urlRegex.test(text) ? text : `/${text}`
+  const urlRegex = /^(https?:\/\/|www\.)[^\s/$.?#].[^\s]*$/i
+  // Check if the text is a full URL
+  if (urlRegex.test(text)) {
+    return text
+  }
+
+  // Check if it's a relative path starting with "/"
+  if (text.startsWith('/')) {
+    return `${getBaseUrl()}${text}`
+  }
+
+  // Otherwise, add the base URL with a leading slash
+  return `${getBaseUrl()}/${text}`
 }
